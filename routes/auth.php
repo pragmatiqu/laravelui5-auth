5676e932516fa1798cdc6db523648138d0b44f47
Michael Gerzabek <michael.gerzabek@gmail.com>
2026-02-07 15:30:27 +0100

    WIP Mit korrekter Middleware

--- routes/auth.php
+++ routes/auth.php
@@ -3,12 +3,16 @@
 use Pragmatiqu\Auth\Controllers\LoginController;
 use Pragmatiqu\Auth\Controllers\LogoutController;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', LoginController::class)
-        ->name('login');
-});
+Route::middleware('web')->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', LogoutController::class)
-        ->name('logout');
+    Route::middleware('guest')->group(function () {
+        Route::get('/login', LoginController::class)
+            ->name('login');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::post('/logout', LogoutController::class)
+            ->name('logout');
+    });
+
 });
